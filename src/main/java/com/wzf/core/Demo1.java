a84c153760df0f04a4537d7fcb7bb4adb89d07b1
wzf <15638180292@163.com>
2020-02-26 12:06:03 +0800

    Java 8 中的 LocalDate 用于表示当天日期。和java.util.Date不同，它只有日期，不包含时间。当你仅需要表示日期时就用这个类。

---
+++ src/main/java/com/wzf/core/Demo1.java
@@ -0,0 +1,13 @@
+package com.wzf.core;
+
+import java.time.LocalDate;
+
+/**
+ * @author w1563
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("今天的日期:" + today);
+    }
+}
