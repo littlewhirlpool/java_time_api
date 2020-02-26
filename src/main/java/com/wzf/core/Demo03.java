b5beb13f643837eb5ff7f6818e4b4c5e2a179ea2
wzf <15638180292@163.com>
2020-02-26 12:12:47 +0800

    Java 8中处理特定日期

---
+++ src/main/java/com/wzf/core/Demo03.java
@@ -0,0 +1,13 @@
+package com.wzf.core;
+
+import java.time.LocalDate;
+
+/**
+ * @author w1563
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2018, 2, 6);
+        System.out.println("自定义日期:" + date);
+    }
+}
